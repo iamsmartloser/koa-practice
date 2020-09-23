c29f042c8a25543e4bde81d616c4fb55e150c9dc
iamsmartloser <121484818@qq.com>
2020-09-23 18:19:04 +0800

    微信小程序登录

--- app/service/user.js
+++ app/service/user.js
@@ -1,4 +1,6 @@
+const util = require('util')
 const bcrypt = require('bcryptjs')
+const axios = require('axios')
 const {User} = require('../models/user');
 const { generateToken } = require('../../core/util')
 const {
@@ -36,16 +38,58 @@ class UserService {
         // 登录类型
         switch (params.type) {
             case LoginType.USER_EMAIL:
-                const user=await this._verifyPassword(params.email,params.password)
+                const user=await this._verifyPassword(params.account,params.password)
                 token =generateToken(user.id, Auth.USER);
                 break;
+            case LoginType.USER_MINI_PROGRAM:
+                token = this.miniProgramLoginVerify(params.account)
+                break
             default:
                 throw new HttpException('没有相应的处理函数')
                 break;
         }
+
         ctx.body = {
             token
         }
+    }
+
+    // 小程序登录验证
+    async miniProgramLoginVerify (code) {
+        const url = util.format(global.config.wx.loginUrl,
+            global.config.wx.appId,
+            global.config.wx.appSecret,
+            code)
+        const result = await axios.get(url)
+        if (result.status !== 200) {
+            throw new HttpException('openid获取失败')
+        }
+        const errcode = result.data.errcode
+        const errmsg = result.data.errmsg
+        if (errcode){
+            throw new HttpException('openid获取失败:'+errmsg)
+        }
+
+        let user = await User.getUserByOpenid(result.data.openid)
+        if(!user){
+            user = await User.registerByOpenid(result.data.openid)
+        }
+        return generateToken(user.id, Auth.USER)
+    }
+
+    async getUserByOpenid(openid){
+        const user = await User.findOne({
+            where:{
+                openid
+            }
+        })
+        return user
+    }
+
+    async registerByOpenid(openid) {
+        return await User.create({
+            openid
+        })
     }
 
     async _verifyPassword(email, plainPassword){
--- config/config.js
+++ config/config.js
@@ -13,4 +13,9 @@ module.exports = {
         secretKey:"abcdefg",
         expiresIn:60*60*24*30,// 过期时间
     },
+    wx:{
+        appId:'wxa954bf263f23a7a6',
+        appSecret:'134d70b05a5ec388b8137ac47adaeaca',
+        loginUrl:'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
+    },
 }
